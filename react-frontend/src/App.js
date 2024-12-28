c0f305961213b15e15a2eb6f9512b48323ddedf0
kristian.ivanov <kristian.ivanov@kristianivanovs-MacBook-Pro.local>
2024-12-28 20:54:40 +0200

    fix menu

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,27 +1,35 @@
 // src/App.js
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Import Router components
 import Tasks from './components/Task';  // Import the Tasks component
 
 function App() {
   return (
-    <div className="App">
-      {/* Navbar/Menu */}
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#">Managment</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#">Tasks</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <Router>
+      <div className="App">
+        {/* Navbar/Menu */}
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Managment System</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link> {/* Link to Home */}
+              <Nav.Link as={Link} to="/tasks">Tasks</Nav.Link> {/* Link to Tasks */}
+            </Nav>
+          </Container>
+        </Navbar>
 
-      {/* Main content */}
-      <div style={{ padding: '20px' }}>
-        <h1>Tasks</h1>
-        <Tasks />  {/* Render the Tasks component */}
+        {/* Main content */}
+        <div style={{ padding: '20px' }}>
+          <h1>To-Do List</h1>
+          {/* Define Routes */}
+          <Routes>
+            <Route path="/" element={<div>Welcome to the Managments!</div>} />  {/* Home page */}
+            <Route path="/tasks" element={<Tasks />} />  {/* Render Tasks component when clicked */}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
